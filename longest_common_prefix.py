9540c2dc7569aafdd170289a3d4285c5ccfc598d
YZCUS <s87610281@gmail.com>
2019-10-14 12:26:54 -0500

    .

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -13,3 +13,16 @@
 # Note:
 # All given inputs are in lowercase letters a-z.
 
+class solution:
+    def prefix(self,strs:[str]) -> str:
+        strslen=[len(strs[a]) for a in range(len(strs))]
+        k=min(strslen)
+        minindex=[p for p,t in enumerate(strslen) if t==k]
+
+        for i in range(k):
+            for j in range(len(strs)):
+                if strs[minindex[0]][i]!=strs[j][i]:
+                    return strs[0][0:i]
+
+print(solution.prefix(solution,["flower","flow","flight"]))
+print(solution.prefix(solution,["dog","racecar","car"]))
