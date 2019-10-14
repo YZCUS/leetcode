f881d07c740ad7a164335a7566e42e18650ea382
YZCUS <s87610281@gmail.com>
2019-10-14 09:19:54 -0500

    .

--- romantointeger.py
+++ romantointeger.py
@@ -30,3 +30,23 @@
 # Input: "MCMXCIV"
 # Output: 1994
 # Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+class solution:
+    def romantointeger(self,x:str)->int:
+        dict={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        sum=0
+        for i in range(len(x)):
+            sum+=dict[x[i]]
+        if "CM" in x:sum-=200
+        if "CD" in x:sum-=200
+        if "XC" in x:sum-=20
+        if "XL" in x:sum-=20
+        if "IX" in x:sum-=2
+        if "IV" in x:sum-=2
+        return sum
+
+print(solution.romantointeger(solution,"III"))
+print(solution.romantointeger(solution,"IV"))
+print(solution.romantointeger(solution,"IX"))
+print(solution.romantointeger(solution,"LVIII"))
+print(solution.romantointeger(solution,"MCMXCIV"))
