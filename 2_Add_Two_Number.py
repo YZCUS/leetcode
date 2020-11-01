3a12a76d4cb1bfa54ce0a754c130a884c83952e5
YZCUS <s87610281@gmail.com>
2020-10-31 19:47:55 -0500

    .

---
+++ 2_Add_Two_Number.py
@@ -0,0 +1,28 @@
+class ListNode():
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    
+def Createlist(n):
+    if n<=0: return False
+    if n==1: return ListNode(1)
+    else:
+        root = ListNode(1)
+        temp=root
+        for i in range(2,n+1):
+            temp.next = ListNode(i)
+            temp=temp.next
+    return root
+
+def printlist(head):
+    p=head
+    while p!=None:
+        print(p.value)
+        p=p.next
+
+def main():
+    print("Create a linklist")
+    head=Createlist(7)
+    printlist(head)
+
+main()
