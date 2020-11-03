bfdca0dc03b0fa26da5f1df51f9d137302bc3843
YZCUS <s87610281@gmail.com>
2020-11-03 13:30:13 -0600

    .

--- 2_Add_Two_Number.py
+++ 2_Add_Two_Number.py
@@ -3,7 +3,7 @@ class ListNode():
         self.value = value
         self.next = None
     
-def Createlist(n):
+def Createlist(n):    #創建節點
     if n<=0: return False
     if n==1: return ListNode(1)
     else:
@@ -14,15 +14,37 @@ def Createlist(n):
             temp=temp.next
     return root
 
-def printlist(head):
+def printlist(head):     #列印鍵表
     p=head
     while p!=None:
         print(p.value,end=' ')
         p=p.next
 
-def main():
+def listlen(head):        #鍵表長度
+    c=0
+    p=head
+    while p!=None:
+        p=p.next
+        c+=1
+    return c
+
+def insert(head,n):
+    if n<1 or n>listlen(head):
+        return
+    p=head
+    for i in range(1,n-1):
+        p=p.next
+    a=raw_input("Enter a value:")
+    t=ListNode(value=a)
+    t.next=p.next
+    p.next=t
+    return head
+
+def main():      #主函數
     print("Create a linklist",end='\n')
     head=Createlist(7)
     printlist(head)
+    print("The length of listnode is ",listlen(head))
 
-main()
+
+main()       #執行主函數
